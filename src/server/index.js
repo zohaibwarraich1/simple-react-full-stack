cafd867258acc9b0c1f693f8252db8d9ffef4576
zohaibwarraich1 <raveeddogar10@gmail.com>
2026-07-21 16:34:28 +0500

    added 500 status code health check

--- src/server/index.js
+++ src/server/index.js
@@ -5,5 +5,6 @@ const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/health', (req, res) => res.status(500).send({ status: 'ERROR', message: 'Intentional 500 error for testing' }));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
